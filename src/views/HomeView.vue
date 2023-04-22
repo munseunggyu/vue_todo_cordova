5f292e3a67cb95caeea2943a51768eb4affe7f70
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-04-22 20:30:03 +0900

    앱으로 데이터 전달

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,23 @@
 <template>
-  <h1>Home go</h1>
+  <div>
+    <h1>Home go</h1>
+    <button @click="goApp">go App</button>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    goApp() {
+      const param = {
+        name: "kim",
+        age: "21",
+        address: "seoul",
+      };
+      webkit.messageHandlers.cordova_iab.postMessage(JSON.stringify(param));
+    },
+  },
+};
 </script>
 
 <style></style>
